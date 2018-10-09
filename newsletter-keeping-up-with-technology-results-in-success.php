3f4ac1447feaf02ff30ef63d791159395fae54e2
foresitegroup <mark@foresitegrp.com>
2018-10-09 13:21:56 -0500

    Add pages

---
+++ newsletter-keeping-up-with-technology-results-in-success.php
@@ -0,0 +1,25 @@
+<?php
+$PageTitle = "Keeping up with Technology Results in Success";
+
+include "header.php";
+?>
+
+Molded Dimensions has added four significant pieces of capital equipment in 2018.  The goal around these purchases was to bring the level of technology up to the best that is currently available, improve our uptime and quality, and to expand our capabilities beyond traditional limits.  To that end, we have made the following purchases...<br>
+<br>
+
+<strong>New Urethane Casting Machine</strong><br>
+Our main casting machine was over 40 years old and performed well for much of the work that went through it.  However, there were limitations in the amount of material that could be cast per minute and there were ergonomic concerns with the machine.  We replaced the machine with a state-of-the-art new machine.  We have increased our filling speed capability by a factor of about three and the resulting parts are cosmetically much improved.  The increased throughput helps us stay on our promised delivery schedule.  The safeguards in the controls make it difficult to either harm the machine or pour parts that are not at the correct resin vs cure ratio.  The new machine also has an articulating pouring head that makes it much easier to take the pouring to the mold versus pushing a heavy mold to the pouring.  This has been a big hit with the operators.  With a strong preventive maintenance plan the machine uptime is predictable and reliable.<br>
+<br>
+
+<strong>New Injection Molding Presses</strong><br>
+We felt it was time to cycle out a couple of our injection presses this year as well.  One of the presses was nearly 20 years old and finding spare parts had become more and more difficult, both for the press and the computer that runs it.  We took the opportunity to upgrade to a new 300 ton machine with all the bells and whistles.  The increased uptime and reliability has been a nice improvement for our manufacturing group.  We liked the new press so well that we ordered another one to replace a "low-tech" injection press that has been sitting nearly dormant for the last few years.  By swapping out this press we feel like we got a bump in capacity and are better utilizing valuable floor space.  Training for our operators was measured in hours as the presses are very operator friendly and only an improvement from what we have been used to running.  The new generation software allow for more control of the meaningful portions of the molding cycle.<br>
+<br>
+
+<strong>Ram Injection Press</strong><br>
+As our business continues to grow we found the need for an additional 24"x24" ram injection press.  Our application is fairly unique, so we found a used press that was reconditioned to our specifications and outfitted with a custom made injection unit.  Although we are still in the commissioning phase with this press, the result will be a 300 ton press with increased shot capacity and a better ergonomic design.  The firm that built the press for us did a good job and we are in the process of making some additional modifications to better suit our needs.  The added capacity will be welcome when we are up and running in October.  We should see some quality improvements in larger parts due to the increased size of the injection unit as well.<br>
+<br>
+
+Tony Lukas<br>
+Chemist
+
+<?php include "footer.php"; ?>
---
+++ newsletter-opportunity-for-strategic-growth.php
@@ -0,0 +1,24 @@
+<?php
+$PageTitle = "Opportunity for Strategic Growth";
+
+include "header.php";
+?>
+
+We have exciting news at Molded Dimensions: we have added an equity partner for our business, Shoreview Industries from Minneapolis. We are excited that our new partner will add expertise and courage for strategic growth that beneﬁts our customers and employees.<br>
+<br>
+
+My father, who was also a thriving manufacturing business owner, always said, "A business needs to constantly grow and innovate, or it will perish." Often small businesses bump up against the $10M sales mark and get stuck because they are unable to move to the next level. We are proud to have grown steadily to over $20M in sales while adding capabilities like tool-making, automated inspection, and urethane machine pouring. We want to be sure we continue to grow and innovate.<br>
+<br>
+
+In addition to our regular 10% organic internal growth, with Shoreview's support we hope to also acquire new capacity and capabilities through an acquisition or two. Over the past ten years, we have looked at lots of small businesses that would be excellent teammates for Molded Dimensions, but we were too chicken to take action for two reasons. First, we were uncomfortable risking the assets of our Employee Owners who owned 36% of MDI, and second, we lacked the experience to buy a company and fold it in. Now in addition to some of my shares, Shoreview has purchased all our ESOP shares and our employees can roll those funds into pretax IRA's where they can grow until retirement. We have also added a generous 401K match so employees can also save for retirement a new way. But most importantly, Shoreview has a fabulous track record of successfully purchasing and linking companies just like ours, so we are very excited to help Molded Dimensions grow with Shoreview's support.<br>
+<br>
+
+We selected Shoreview because they share our midwestern values. They are committed to growing employment in our current location. Their debt position is extremely conservative, which will provide security for our employees and customers through future economic downturns. They are highly recommended in our local business community as ethical, patient, strategic, helpful partners, and we are thrilled to have them on board.<br>
+<br>
+
+Mike and I, as well as the entire management team, remain in our current roles and have equity stakes in the company. Additionally, I remain on the board of directors. We look forward to seeing how our new structure will allow Molded Dimensions to grow with you. If you have ideas for a technology or capability we should target to beneﬁt your business, please let us know.<br>
+<br>
+
+Sincerely, Linda Katz
+
+<?php include "footer.php"; ?>
