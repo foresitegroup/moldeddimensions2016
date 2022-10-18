354498d4b713b6a285541e619e97eaf6b1aac97e
Mark Lippert <mark@foresitegrp.com>
2022-10-18 10:09:18 -0500

    Remove Wordpress update emails

--- blog/wp-content/themes/moldeddimensions/functions.php
+++ blog/wp-content/themes/moldeddimensions/functions.php
@@ -1,4 +1,14 @@
 <?php
+// Don't send emails when Wordpress updates happen
+add_filter('auto_core_update_send_email', 'stop_auto_update_emails', 10, 4);
+function stop_update_emails($send, $type, $core_update, $result) {
+  if (!empty($type) && $type == 'success') return false;
+  return true;
+}
+add_filter('auto_plugin_update_send_email', '__return_false');
+add_filter('auto_theme_update_send_email', '__return_false');
+
+
 // Enable feature images
 add_theme_support('post-thumbnails');
 
